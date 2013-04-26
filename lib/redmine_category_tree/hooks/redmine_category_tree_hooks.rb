3f31890c70d24f7c74d794275bcd3725658b4e98
unknown <pattebre@GMD-JK4CBS1.MSS.local>
2013-04-26 11:37:35 -0400

    Removing unnecessary method

--- lib/redmine_category_tree/hooks/redmine_category_tree_hooks.rb
+++ lib/redmine_category_tree/hooks/redmine_category_tree_hooks.rb
@@ -6,20 +6,6 @@ module RedmineCategoryTree
 
 				css
 			end
-
-			def helper_issues_show_detail_after_setting(context = {})
-				# if context[:detail].property == 'attr' && context[:detail].prop_key == 'category_id'
-				#	if !context[:detail].old_value.blank?
-				#		oldCategory = IssueCategory.find(context[:detail].old_value)
-				#		context[:detail].old_value = IssueCategory.render_issue_category_with_tree_inline(oldCategory)
-				#	end
-
-				#	if !context[:detail].value.blank?
-				#		newCategory = IssueCategory.find(context[:detail].value)
-				#		context[:detail].value = IssueCategory.render_issue_category_with_tree_inline(newCategory)
-				#	end
-				# end
-			end
 		end
 	end
 end
