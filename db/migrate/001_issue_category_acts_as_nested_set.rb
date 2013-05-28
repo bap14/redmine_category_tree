0ef07e8d2fbc39b309d2df8f320a6c186fd12508
bpat1434 <github@propulsiongel.com>
2013-05-28 16:41:19 -0400

    Adding check to see if migration(s) need to happen for clean install with PGSQL backend - References #2

--- db/migrate/001_issue_category_acts_as_nested_set.rb
+++ db/migrate/001_issue_category_acts_as_nested_set.rb
@@ -5,7 +5,10 @@ class IssueCategoryActsAsNestedSet < ActiveRecord::Migration
 	add_column :issue_categories, :lft, :integer, :null => true
 	add_column :issue_categories, :rgt, :integer, :null => true
 
-	IssueCategory.rebuild!
+	categories = IssueCategory.count
+	if categories > 0
+		IssueCategory.rebuild!
+	end
   end
 
   def self.down
